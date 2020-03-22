9f18139b3e2999e4cf63c56ae006778ccc1a6415
Jeremy Philipson <jeremyphilipson@gmail.com>
2020-03-21 21:51:06 -0400

    chore: make card flippy

--- src/components/ExperienceCard/ExperienceCard.js
+++ src/components/ExperienceCard/ExperienceCard.js
@@ -2,7 +2,15 @@ import React from 'react'
 import { DataItem } from '../../components/shared'
 import { joinString } from '../../helpers'
 import Image from '../Image'
-import { Container, Info, LogoContainer, Title } from './styles'
+import {
+  Container,
+  Info,
+  LogoContainer,
+  Title,
+  InnerContainer,
+  CardFront,
+  CardBack
+} from './styles'
 
 const ExperienceCard = ({ experience }) => {
   const { title, location, company, startDate, endDate } = experience
@@ -14,22 +22,29 @@ const ExperienceCard = ({ experience }) => {
   }
   return (
     <Container>
-      <Title>{title}</Title>
-      <Info>
-        <DataItem value={location} label="Location" {...dataProps} />
-        <DataItem value={company} label="Company" {...dataProps} />
-        <DataItem
-          value={`${startDate} - ${endDate}`}
-          label="Dates"
-          {...dataProps}
-        />
-      </Info>
-      <LogoContainer>
-        <Image
-          queryKey={`${normalizedCompany}Logo`}
-          style={{ width: '60px' }}
-        />
-      </LogoContainer>
+      <InnerContainer>
+        <CardFront>
+          <Title>{title}</Title>
+          <Info>
+            <DataItem value={location} label="Location" {...dataProps} />
+            <DataItem value={company} label="Company" {...dataProps} />
+            <DataItem
+              value={`${startDate} - ${endDate}`}
+              label="Dates"
+              {...dataProps}
+            />
+          </Info>
+          {/* <LogoContainer>
+            <Image
+              queryKey={`${normalizedCompany}Logo`}
+              style={{ width: '60px' }}
+            />
+          </LogoContainer> */}
+        </CardFront>
+        <CardBack>
+          <h3>this is the back of the card</h3>
+        </CardBack>
+      </InnerContainer>
     </Container>
   )
 }
