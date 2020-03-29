ee0e3d38fe03c85ab077ad53aa33ea61e92bc5f7
Jeremy Philipson <jeremyphilipson@gmail.com>
2020-03-29 15:08:11 -0400

    chore: add test for footer

---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Footer from './Footer'
+
+describe('Footer', () => {
+  const getOutput = () => renderer.create(<Footer />)
+
+  it('Renders correctly', () => {
+    const tree = getOutput().toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Displays the copyright, current year, and full name', () => {
+    const instance = getOutput().toJSON()
+    expect(instance.type).toBe('footer')
+    expect(instance.props).toEqual({})
+    expect(instance.children.map(str => str.trim())).toEqual([
+      '©',
+      '2020',
+      'Jeremy L. Philipson'
+    ])
+  })
+})
